105ba3e55e890648c70e7260f49fca801c62e022
Piyush Aggarwal <piyushaggarwal801@gmail.com>
2022-04-09 11:53:45 +0530

    first POST request

---
+++ src/components/Form.jsx
@@ -0,0 +1,12 @@
+function Form({ addNote, newNote, handleNoteChange }) {
+  return (
+    <>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+    </>
+  )
+}
+
+export default Form
